59349e4d89a3dd197e4010f86547ab89a5ca6b73
DongyeonSeo <seodye04@gmail.com>
2022-07-25 23:25:56 +0900

    건물 오브젝트 Pool

--- Assets/Scripts/Item/CollectableObject.cs
+++ Assets/Scripts/Item/CollectableObject.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 // 채집 가능 오브젝트
@@ -12,6 +13,28 @@ public class CollectableObject : MonoBehaviour
         if (needItem == null || InventoryManager.Instance.CurrentItem() == needItem)
         {
             gameObject.SetActive(false);
+
+            if (item.isBuildable) // 건설 가능한 오브젝트라면
+            {
+                // Key가 없으면 새로 만들고, 사용 가능을 false로 바꿈
+                if (!BuildManager.Instance.isUsableBuilding.ContainsKey(item))
+                {
+                    BuildManager.Instance.isUsableBuilding.Add(item, false);
+                }
+                else
+                {
+                    BuildManager.Instance.isUsableBuilding[item] = false;
+                }
+
+                // Key가 없으면 새로 만듦
+                if (!BuildManager.Instance.poolObjectDictionary.ContainsKey(item))
+                {
+                    BuildManager.Instance.poolObjectDictionary.Add(item, new Queue<GameObject>());
+                }
+
+                // Pool Dictionary에 집어 넣음
+                BuildManager.Instance.poolObjectDictionary[item].Enqueue(gameObject);
+            }
 
             return item;
         }
--- Assets/Scripts/Manager/BuildManager.cs
+++ Assets/Scripts/Manager/BuildManager.cs
@@ -8,6 +8,8 @@ public class BuildManager : MonoBehaviour // 건설 Manager
     
     // 사용 가능한 건설 오브젝트 확인
     public Dictionary<ItemSO, bool> isUsableBuilding = new Dictionary<ItemSO, bool>();
+    // Pool 오브젝트 Dictionary
+    public Dictionary<ItemSO, Queue<GameObject>> poolObjectDictionary = new Dictionary<ItemSO, Queue<GameObject>>();
 
     // 싱글톤 패턴
     private static BuildManager instance;
@@ -44,6 +46,30 @@ public class BuildManager : MonoBehaviour // 건설 Manager
         // 오브젝트를 플레이어 앞쪽에 배치하도록 설정
         Vector3 position = GameManager.player.position + rotation * currentBuildItem.buildPosition;
         // 오브젝트 생성
-        Instantiate(currentBuildItem.buildObject, position, rotation);
+
+        // Key가 없다면 만듦
+        if (!poolObjectDictionary.ContainsKey(currentBuildItem))
+        {
+            poolObjectDictionary.Add(currentBuildItem, new Queue<GameObject>());
+        }
+
+        GameObject buildObject;
+
+        if (poolObjectDictionary[currentBuildItem].Count > 0)
+        {
+            // Pool Object에서 가져옴
+            buildObject = poolObjectDictionary[currentBuildItem].Dequeue();
+        }
+        else
+        {
+            // 없으면 새로 만듦
+            buildObject = Instantiate(currentBuildItem.buildObject);
+        }
+
+        // 위치와 방향 설정
+        buildObject.transform.position = position;
+        buildObject.transform.rotation = rotation;
+
+        buildObject.SetActive(true);
     }
 }
